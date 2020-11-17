1e9c64d6aac6cc1c179901966997ed283336a64f
xuao <xuao@si-tech.com.cn>
2020-11-17 16:58:34 +0800

    [新增功能](1.0.1): elasticjob@xuao
    elasticjob@xuao

---
+++ elastic-job/src/test/java/com/xudashu/elasticjob/ElasticJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xudashu.elasticjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
